57c26e01cc7cad57daf13011e90b07bafd3adb12
Gabriel-de-Albuquerque-dos-Santos <albuquerque99gabriel@gmail.com>
2026-03-20 14:51:57 +0000

    feat: Implementado o Layout.jsx e renomeado o arquivo Header.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,9 +5,18 @@ export default function Layout({ children }) {
   return (
     <div>
       <Header />
-      <main style={{ padding: "1rem" }}>
+      
+      <main style={{ padding: "1rem", minHeight: "80vh" }}>
         {children}
       </main>
+
+      <footer style={{ 
+        textAlign: "center", 
+        padding: "1rem", 
+        borderTop: "1px solid #ccc" 
+      }}>
+        <p>© 2025 Pousada Pedra Furada</p>
+      </footer>
     </div>
   );
 }
